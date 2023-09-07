2cf5a239b1b24759f589b20819b99be8f9a79408
Rajarshi Bandopadhyay <theocrat93@gmail.com>
2023-09-07 18:44:02 +0530

    Implemented this file. Finally!

---
+++ js/tojson.js
@@ -0,0 +1,27 @@
+function generateRecursively(root) {
+    if (root.type == "null") { return null }
+    if (root.type == "boolean") { return root.value }
+    if (root.type == "int")     { return root.value }
+    if (root.type == "float")   { return root.value }    
+    if (root.type == "string")  { return root.value }
+
+    if (root.type == "array") { 
+        return root.value.map(item => generateRecursively(item)) 
+    }
+
+    if (root.type == "object") { 
+        let values = {}
+        for (let key in root.value) {
+            values[key] = generateRecursively(root.value[key])
+        } 
+        return values
+    }
+}
+
+function generateJSON() {
+    let rootNode = createdObjects[0]
+    let outputJSON = generateRecursively(rootNode)
+    
+    console.table(outputJSON)
+    output.innerHTML = JSON.stringify(outputJSON, null, 2)
+}
