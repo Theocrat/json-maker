9dff6cd04437dd1bfe615e5e34921ebbf0539fb7
Rajarshi Bandopadhyay <theocrat93@gmail.com>
2023-09-07 17:54:56 +0530

    Implemented Component Placement

---
+++ js/events.js
@@ -0,0 +1,90 @@
+function selectNode(id) {
+    let selectMode = interactiveState.type == "select" 
+    let changeMode = interactiveState.type == "change"
+    if (selectMode || changeMode) {
+        if (interactiveState.type == "change") {
+            deselectAll()
+        }
+        interactiveState.type = "change"
+        interactiveState.target = createdObjects[id]
+        createdObjects[id].selected = true
+        createdObjects[id].update()
+    }
+}
+
+function deselectAll() {
+    createdObjects.forEach(node => {
+        node.selected = false
+        node.update()
+    })
+    
+    if (interactiveState.type == "change") {
+        interactiveState.type = "select"
+        interactiveState.target = "none"
+    }
+}
+
+
+function place(type) {
+    if (interactiveState.type != "change") {
+        return
+    }
+
+    let selectedItem  = interactiveState.target
+    let promptedKey   = null
+    let promptedValue = null
+
+    let arraySelected  = selectedItem.type == "array"
+    let objectSelected = selectedItem.type == "object"
+    if (!arraySelected && !objectSelected) {
+        return
+    }
+
+    if (selectedItem.type == "object") {
+        promptedKey = prompt("Enter the KEY to which this child will be mapped")
+    }
+    
+    if (type == "boolean") {
+        let t_or_f = prompt("Enter T for true, F for false")
+        if (t_or_f.toLowerCase() == "t") {
+            promptedValue = true
+        }
+    }
+
+    if (type == "int") {
+        promptedValue = parseInt(prompt("Enter the integer value"))
+    }
+
+    if (type == "float") {
+        promptedValue = parseFloat(prompt("Enter the float value"))
+    }
+
+    if (type == "string") {
+        promptedValue = prompt("Enter the string value")
+    }
+    
+    interactiveState.type = "place"
+    interactiveState.target = {
+        "parent": selectedItem,
+        "type":   type,
+        "key":    promptedKey,
+        "value":  promptedValue
+    }
+    
+    deselectAll()
+}
+
+function createNode() {
+    if (interactiveState.type == "place") {
+        let parent = interactiveState.target.parent
+        let type   = interactiveState.target.type
+        let key    = interactiveState.target.key
+        let value  = interactiveState.target.value
+
+        interactiveState.type   = "select"
+        interactiveState.target = "none"
+
+        let newNode = new DndNode(parent, type, key, value)
+        createdObjects.push(newNode)
+    }
+}
--- js/init.js
+++ js/init.js
@@ -1,4 +1,25 @@
 function readyDndArea() {
     area = document.querySelector("#constructionArea")
-    area.addEventListener("mousemove", function (event) )
+
+    area.addEventListener("mousemove", function (event) {
+        mouse.x = event.offsetX
+        mouse.y = event.offsetY
+        
+        let hoverNode = document.querySelector("#hoverNode")
+        if (interactiveState.type == "place") {
+            if (hoverNode.style.visibility != "visible") {
+                hoverNode.style.visibility = "visible"
+            }
+            hoverNode.x.baseVal.value  = mouse.x
+            hoverNode.y.baseVal.value  = mouse.y
+        }
+        else {
+            hoverNode.style.visibility = "hidden"
+        }
+    } )
+
+    let rootObject = new DndNode(null, "object", null, {})
+    rootObject.x = area.width.baseVal.value  / 2
+    rootObject.y = area.height.baseVal.value / 2
+    rootObject.update()
 }
